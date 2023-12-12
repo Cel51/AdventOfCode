0d42ddef97b9de56260f1c893353bc154827fba8
Célien Heritier <celien.heritier@gmail.com>
2023-12-12 14:11:35 +0100

    feat: contd.

--- 2023/day_5/main_2.py
+++ 2023/day_5/main_2.py
@@ -7,19 +7,32 @@ seeds = re.findall(r"(\d+) (\d+)", items[0])
 # convert to array of array of ints
 seeds = list(map(lambda x: list(map(int, x)), seeds))
 
-print(seeds)
+seed_list = []
+for m in seeds:
+    start, length = m
+    for seed in range(start, start + length):
+        seed_list.append({
+            'origin': start,
+            'seed': seed
+        })
+
+print(seed_list)
 
 min_location = float("inf")
+origin_seed = ''
 
-for seed_start, seed_length in seeds:
-    for seed in range(seed_start, seed_start + seed_length):
-        print(seed)
-    #     for item in items[1:]:
-    #         for values in re.findall(r"(\d+) (\d+) (\d+)", item):
-    #             destination, start, length = map(int, values)
-    #             if seed in range(start, start + length):
-    #                 seed += destination - start
-    #                 break
-    # min_location = min(min_location, seed)
+for seed in seed_list:
+    print(seed)
+    print(seed.get('seed'))
+    for item in items[1:]:
+        for values in re.findall(r"(\d+) (\d+) (\d+)", item):
+            destination, start, length = map(int, values)
+            if seed.get('seed') in range(start, start + length):
+                seed['seed'] += destination - start
+                print(seed)
+                break
+    if min_location > seed.get('seed'):
+        min_location = seed.get('seed')
+        origin_seed = seed.get('origin')
 
-# print(min_location)
+print(origin_seed)
