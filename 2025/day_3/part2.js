93e0ad873d7865b925af1c557844d27f7ed502a2
Célien Heritier <celien.heritier@gmail.com>
2026-03-17 09:14:48 +0000

    2025 day_3 part2

--- 2025/day_3/part2.js
+++ 2025/day_3/part2.js
@@ -3,28 +3,58 @@ const path = require('path');
 
 const filePath = path.join(__dirname, 'entry.txt');
 
-const lines = fs.readFileSync(filePath, 'utf-8')
-    .split(',')
+const stack = fs.readFileSync(filePath, 'utf-8')
+    .split('\n')
     .map((line, idx) => {
-        return line.split('-').map(num => parseInt(num, 10));
+        return line.split('').map(num => parseInt(num, 10));
     });
+
 
 let sum = 0;
 
-for (let i = 0; i < lines.length; i++) {
-    const [start, end] = lines[i];
-    for (let j = start; j <= end; j++) {
-        // create a regex for all repeating groups of digits of length 1 to numdigits/2
-        const numDigits = j.toString().length;
-        for (let groupLength = 1; groupLength <= numDigits / 2; groupLength++) {
-            const regex = new RegExp(`^(\\d{${groupLength}})\\1+$`);
-            if (regex.test(j.toString())) {
-                console.log('Found a number with repeating groups of digits: ' + j);
-                sum = sum + j;
-                break; // break out of the loop once we find a match
-            }
-        }
+stack.forEach((row, idx) => {
+    sum += getMaxValue(row, 12);
+});
+
+console.log('MaxValue: ' + sum);
+
+function getMaxValue(bankNumbers, countToKeep) {
+    let bankNumbersLength = bankNumbers.length;
+    let numbersToRemove = bankNumbersLength - countToKeep;
+    let results = [];
+    let deleteCounter = 0;
+
+    if(numbersToRemove === 0) {
+        return parseInt(bankNumbers.join(''), 10);
     }
+
+    if(countToKeep === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < bankNumbersLength; i++) {
+        let actualNumber = bankNumbers[i];
+        let stillInBankNumbers = bankNumbersLength - i;
+        let requiredNumbersToAdd = countToKeep - results.length;
+
+        if (stillInBankNumbers === requiredNumbersToAdd) {
+            results.push(...bankNumbers.slice(i));
+            break;
+        }
+
+        while (results.length > 0 && deleteCounter < numbersToRemove && results[results.length - 1] < actualNumber) {
+            results.pop();
+            deleteCounter++;
+        }
+
+        results.push(actualNumber);
+    }
+
+    while (deleteCounter < numbersToRemove) {
+        results.pop();
+        deleteCounter++;
+    }
+
+    return parseInt(results.join(''), 10);
 }
 
-console.log('The sum of all the numbers with 2 repeating groups of digits is: ' + sum);
